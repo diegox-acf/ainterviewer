8dc2a54a3618148072f6769d3a365da971a18a68
Diegox <diego.adrian.777@gmail.com>
2025-05-05 04:25:54 -0400

    fix security

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiwGOmBTPOzua7X0_MktHg5wuB5QuD8tk",
-  authDomain: "ainterviewer-16412.firebaseapp.com",
-  projectId: "ainterviewer-16412",
-  storageBucket: "ainterviewer-16412.firebasestorage.app",
-  messagingSenderId: "667244230897",
-  appId: "1:667244230897:web:95c4fb2b7ef8bb9e3cd150",
-  measurementId: "G-GNB3YJ7582",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
