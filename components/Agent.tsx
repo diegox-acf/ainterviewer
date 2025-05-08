e1cbbd1701993ed623bf3a110f9c0ec8effa6f25
Diegox <diego.adrian.777@gmail.com>
2025-05-08 04:54:13 -0400

    show the generated interviews on the homepage

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -66,8 +66,8 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
     setCallStatus(CallStatus.CONNECTING);
     await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
       variableValues: {
-        username: userName,
-        userid: userId,
+        userName,
+        userId,
       },
     });
   };
